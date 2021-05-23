a51eff42c79e5eb839cc6016790e53fb4d8dc64f
gvssmark <83894487+gvssmark@users.noreply.github.com>
2021-05-23 20:33:21 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,15 @@
-self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('airhorner').then(function(cache) {
-     return cache.addAll([
-       '/',
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('fox-store').then((cache) => cache.addAll([
+      '/',
        '/index.html',
-     ]);
-   })
- );
-});
-
-// Cache and return requests
-self.addEventListener("fetch", e=>{
-  e.respondWith(
-      caches.match(e.request).then(response=>{
-          return response || fetch(e.request);
-      })
+       ])),
   );
 });
 
-// Update a service worker
-const cacheWhitelist = ['Simon-game'];
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+self.addEventListener('fetch', (e) => {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
   );
 });
