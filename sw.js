d6e74c3a6edb71f726ba7679e2df2b990f5f7d70
gvssmark <83894487+gvssmark@users.noreply.github.com>
2021-05-24 06:30:55 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,8 +3,7 @@ self.addEventListener('install', (e) => {
     caches.open('fox-store').then((cache) => cache.addAll([
       '/',
        '/index.html',
-      '/images/i192.png',
-   '/images/i512.png'
+      'images/i192.png',
       ])),
   );
 });
