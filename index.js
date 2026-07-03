bfefe938903b1be27bc2dddc5844ec93fa87be0c
gvssmark <83894487+gvssmark@users.noreply.github.com>
2026-07-03 15:30:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('sw.js')
+    .register('/sw.js')
     .then(() => { console.log('Service Worker Registered'); });
 }
 
